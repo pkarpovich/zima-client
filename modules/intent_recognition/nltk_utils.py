41229461b4c944f4ab305ebe3edfa852c3e12396
Pavel Karpovich <pavel.karpovich@outlook.com>
2023-02-18 23:08:41 +0100

    wip: add intent recognition module

---
+++ modules/intent_recognition/nltk_utils.py
@@ -0,0 +1,41 @@
+import numpy as np
+import nltk
+
+nltk.download('punkt')
+nltk.download('stopwords')
+
+from pymystem3 import Mystem
+from nltk.stem.porter import PorterStemmer
+from nltk.corpus import stopwords
+from string import punctuation
+
+mystem = Mystem()
+stemmer = PorterStemmer()
+stop_words = set(stopwords.words("russian"))
+
+
+def tokenize(sentence):
+    return nltk.word_tokenize(sentence)
+
+
+def stem(word):
+    return stemmer.stem(word.lower())
+
+
+def bag_of_words(tokens, words):
+    # stem each word
+    tokens = [stem(word) for word in tokens]
+    tokens = [token for token in tokens if token.strip() not in punctuation]
+    tokens = [token for token in tokens if not token.lower() in stop_words]
+
+    # sentence_words = [token for token in sentence_words if token not in russian_stopwords \
+    #           and token != " " \
+    #           and token.strip() not in punctuation]
+
+    # initialize bag with 0 for each word
+    bag = np.zeros(len(words), dtype=np.float32)
+    for idx, w in enumerate(words):
+        if w in tokens:
+            bag[idx] = 1
+
+    return bag
